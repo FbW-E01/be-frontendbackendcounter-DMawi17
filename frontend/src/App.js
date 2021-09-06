bf0ad192a143182d3552cdd79afaa269ec778b8e
DMawi17 <asegda17@gmail.com>
2021-09-06 18:40:48 +0200

    add front and back end

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+    const [counter, setCounter] = useState(0);
+    const url = "http://localhost:3000/";
+
+    //...
+
+    const handleGetJson = () => {
+        fetch(url, { method: "GET" })
+            .then((res) => res.json())
+            .then((json) => setCounter(json.counter));
+    };
+
+    //...
+
+    const handlePostJson = () => {
+        fetch(url, { method: "POST" });
+    };
+
+    //...
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={handleGetJson}>
+                    GET: <span>{counter}</span>
+                </button>
+                <br />
+                <button onClick={handlePostJson}>POST</button>
+            </header>
+        </div>
+    );
+}
+
+export default App;
